1cdf6981e950685e17eded956f8a236bc4b3b563
Himphen <himphen@gmail.com>
2021-06-24 04:53:46 +0800

    feat: fetch weather from firebase database

--- app/src/main/java/hibernate/v2/sunshine/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/hibernate/v2/sunshine/ui/weather/WeatherViewModel.kt
@@ -2,6 +2,12 @@ package hibernate.v2.sunshine.ui.weather
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 import com.himphen.logger.Logger
 import hibernate.v2.api.model.openweather.OneCall
 import hibernate.v2.sunshine.api.DataRepository
@@ -14,6 +20,28 @@ class WeatherViewModel(private val repo: DataRepository) : BaseViewModel() {
     val oneCallData = MutableLiveData<OneCall>()
 
     fun getWeatherInfo() {
+        Logger.d("lifecycle getWeatherInfo")
+        viewModelScope.launch {
+            val database = Firebase.database
+            val weatherRef = database.getReference("weather")
+            weatherRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val value = dataSnapshot.getValue<OneCall>()
+                    value?.let {
+                        oneCallData.postValue(it)
+                    } ?: run {
+                        Logger.w("value == null.")
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Logger.e(error.toException(), "Failed to read value.")
+                }
+            })
+        }
+    }
+
+    fun getWeatherInfoFromApi() {
         Logger.d("lifecycle getWeatherInfo")
         viewModelScope.launch {
             try {
