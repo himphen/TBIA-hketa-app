9bf3216cabe41a175fe00087c40b12b028f412bb
Himphen <himphen@gmail.com>
2021-08-13 19:10:40 +0800

    feat: route id

--- app/src/main/java/hibernate/v2/sunshine/ui/eta/mobile/view/EtaViewHolderClassic.kt
+++ app/src/main/java/hibernate/v2/sunshine/ui/eta/mobile/view/EtaViewHolderClassic.kt
@@ -1,13 +1,14 @@
 package hibernate.v2.sunshine.ui.eta.mobile.view
 
 import android.graphics.drawable.GradientDrawable
-import android.view.View
+import android.graphics.drawable.LayerDrawable
 import hibernate.v2.sunshine.R
 import hibernate.v2.sunshine.databinding.ItemEtaClassicBinding
 import hibernate.v2.sunshine.model.Card
 import hibernate.v2.sunshine.model.transport.LRTTransportRoute
 import hibernate.v2.sunshine.model.transport.MTRTransportRoute
 import hibernate.v2.sunshine.ui.eta.EtaTimeAdapter
+import hibernate.v2.sunshine.util.dpToPx
 import hibernate.v2.sunshine.util.gone
 import hibernate.v2.sunshine.util.visible
 
@@ -22,6 +23,7 @@ class EtaViewHolderClassic(viewBinding: ItemEtaClassicBinding) :
             if (route is MTRTransportRoute) {
                 routeNumberTv.gone()
                 routeCompanyColor.gone()
+                routeLRTNumberTv.gone()
                 routeMTRNumberTv.apply {
                     text = card.platform
                     visible()
@@ -31,6 +33,21 @@ class EtaViewHolderClassic(viewBinding: ItemEtaClassicBinding) :
                         setColor(color)
                     }
                 }
+            } else if (route is LRTTransportRoute) {
+                routeNumberTv.gone()
+                routeCompanyColor.gone()
+                routeMTRNumberTv.gone()
+                routeLRTNumberTv.apply {
+                    text = card.route.routeNo
+                    visible()
+
+                    (background as? LayerDrawable)?.apply {
+                        mutate()
+                        (findDrawableByLayerId(R.id.item_line_color) as? GradientDrawable)?.apply {
+                            setStroke(dpToPx(6), color)
+                        }
+                    }
+                }
             } else {
                 routeNumberTv.apply {
                     text = card.route.getCardRouteText()
@@ -38,6 +55,7 @@ class EtaViewHolderClassic(viewBinding: ItemEtaClassicBinding) :
                 }
                 routeCompanyColor.visible()
                 routeMTRNumberTv.gone()
+                routeLRTNumberTv.gone()
                 routeCompanyColor.setBackgroundColor(color)
             }
 
